da62dd1e91b55709d1992dd36fd8e2756c7ec26d
GandraNNA <praskovia.gandrabura@yandex.ru>
2022-06-27 23:52:01 +0300

    add serializers

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from posts.models import Group, Post, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = "__all__"
+
+
+class PostSerializer(serializers.ModelSerializer):
+    group = serializers.SlugRelatedField(
+        slug_field='title',
+        queryset=Group.objects.all(),
+        required=False
+    )
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    publication_date = serializers.DateTimeField(
+        source='pub_date',
+        read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Post
