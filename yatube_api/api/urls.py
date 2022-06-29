81f5a3a530d165f1cfb6d63ec9683a66284e2ffb
GandraNNA <praskovia.gandrabura@yandex.ru>
2022-06-30 00:30:55 +0300

    edit router name and transfer api-token-auth

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,27 +1,29 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from rest_framework.authtoken import views
+
 
 from .views import PostViewSet, CommentViewSet, GroupViewSet
 
+router_v1 = SimpleRouter()
 
-router = SimpleRouter()
-
-router.register(
+router_v1.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
-router.register(
+router_v1.register(
     r'groups',
     GroupViewSet,
     basename='groups'
 )
-router.register(
+router_v1.register(
     r'posts',
     PostViewSet,
     basename='posts'
 )
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
